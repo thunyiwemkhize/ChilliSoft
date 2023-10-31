c82632e0c56c4afa46674471af0ea6d983519b28
thunyiwep <thunyiwepmkhize@gmail.com>
2023-10-31 19:27:57 +0200

    work on finishing up generics

---
+++ Chillisoft.profile/Junior.one/Inheritance/Person.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace Junior.one.Inharitance
+
+{
+    public class Person
+    {
+       public Guid Id { get; set; }
+       public string FullName { get; set; }
+
+       public Person()
+       {
+           FullName = string.Empty;
+       }
+    }
+}
---
+++ Chillisoft.profile/Junior.one/TestPerson.cs
@@ -0,0 +1,45 @@
+﻿using Junior.one.Inharitance.Repository;
+using System.Text.Json;
+
+namespace Junior.one
+{
+    public class TestPerson
+    {
+        public void testPerson()
+        {
+
+            var repo = new GenericRepository<Staff>();
+            var studentRepo = new GenericRepository<Student>();
+
+            Console.WriteLine("enter staff fullname");
+            var staffName = Console.ReadLine();
+            Console.WriteLine("Enter staff number");
+            var staffNumber = Console.ReadLine();
+            var staf = new Staff()
+            {
+                FullName = staffName!,
+                Position = "position",
+                StaffNumber = staffNumber!,
+                Id = Guid.NewGuid()
+            };
+
+            Console.WriteLine("Enter student fullname");
+            var studentName = Console.ReadLine();
+            Console.WriteLine("Enter student number");
+            var studentNumber = Console.ReadLine();
+            var student = new Student()
+            {
+                Id = Guid.NewGuid(),
+                Course = "ND: IT",
+                FullName = studentName,
+                StudentNumber = studentNumber,
+            };
+            repo.Create(staf);
+            studentRepo.Create(student);
+
+            Console.WriteLine(JsonSerializer.Serialize(repo.GetData()));
+            Console.WriteLine(JsonSerializer.Serialize(studentRepo.GetData()));
+
+        }
+    }
+}

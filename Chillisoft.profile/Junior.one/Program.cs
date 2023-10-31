600e0bdb1c9704a7bb2c9ede7ffb2be6ebe1f873
thunyiwep <thunyiwepmkhize@gmail.com>
2023-10-31 19:53:48 +0200

    work static variables

--- Chillisoft.profile/Junior.one/Program.cs
+++ Chillisoft.profile/Junior.one/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Junior.one;
+using Junior.one.Statics;
 
-new TestPerson().testPerson();
+//new TestPerson().testPerson();
+new DifferenceBetweenStaticAndNonStaticVariableWritter().write();
+new StaticVariables().write();
---
+++ Chillisoft.profile/Junior.one/Statics/StaticVariables.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Junior.one.Statics
+{
+    public class StaticVariables
+    {
+        public static string name = "mode1";
+        public string nonStatic = "mode2";
+        public void write()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(nonStatic);
+        }
+    }
+
+    public class DifferenceBetweenStaticAndNonStaticVariableWritter
+    {
+        public void write()
+        {
+            Console.WriteLine($"Current value for the static variable is {StaticVariables.name}");
+            StaticVariables.name = "thunyiwe";
+            var staticFeild = new StaticVariables()
+            {
+                nonStatic ="non static"
+            };
+            Console.WriteLine(StaticVariables.name);
+            Console.WriteLine(staticFeild.nonStatic);
+        }
+    }
+}
